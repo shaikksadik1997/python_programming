c9e188878d252d6acb9ef5079c110bd2f3773920
shaikksadik1997 <shaikksadik1997@outlook.com>
2025-01-01 22:37:31 +0530

    including black formatter

--- Beginner/introduction.py
+++ Beginner/introduction.py
@@ -1,4 +1,3 @@
 from __future__ import print_function
 
-print("Hello Guys!!\nWelcome to the world of Python programming\n"
-      "This is the first program in Python")
+print("Hello Guys!!\nWelcome to the world of Python programming\nThis is the first program in Python")
