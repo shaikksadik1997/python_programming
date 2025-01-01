a713bb3e7850d54c7af326255e97725dd64ffa72
shaikksadik1997 <shaikksadik1997@outlook.com>
2025-01-01 21:39:44 +0530

    simplifying the print statements

--- Beginner/introduction.py
+++ Beginner/introduction.py
@@ -1,4 +1,2 @@
 from __future__ import print_function
-print("Hello Guys!!")
-print("Welcome to the world of Python programming")
-print("This is the first program in Python")
+print("Hello Guys!!\nWelcome to the world of Python programming\nThis is the first program in Python")
