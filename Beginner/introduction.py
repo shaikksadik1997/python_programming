9ea5d9a2b5a76d98a09e373aa8ab335cc4f69065
shaikksadik1997 <shaikksadik1997@outlook.com>
2025-01-01 22:07:13 +0530

    Fixed the issues in introduction.py file

--- Beginner/introduction.py
+++ Beginner/introduction.py
@@ -1,2 +1,4 @@
 from __future__ import print_function
-print("Hello Guys!!\nWelcome to the world of Python programming\nThis is the first program in Python")
+
+print("Hello Guys!!\nWelcome to the world of Python programming\n"
+      "This is the first program in Python")
