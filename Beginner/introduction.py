108f6664fc49886bd89ae7834625457b612940a7
Shaik Sadik <shaikksadik1997@outlook.com>
2025-01-02 17:14:20 +0530

    Update introduction.py

--- Beginner/introduction.py
+++ Beginner/introduction.py
@@ -2,4 +2,4 @@ from __future__ import print_function
 
 print("Hello Guys!!\n"
       "Welcome to the world of Python programming\n"
-      "This is the first program in Python")
+      "This is the first program in Python and integrated code rabbit")
